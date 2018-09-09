b35772846a92bf66ba402c27228a8d1ef6aa2b84
Calle <calle.strom@hotmail.com>
2018-09-09 20:38:54 +0200

    set on/off time for evening lamp

--- appdaemon/apps/evening_lamp.py
+++ appdaemon/apps/evening_lamp.py
@@ -1,10 +1,15 @@
 import appdaemon.plugins.hass.hassapi as hass
+import datetime
 
 class EveningLamp(hass.Hass):
 
   def initialize(self):
-    self.run_at_sunset(self.light_on, offset=int(self.args["sunset_offset"])
-    self.run_daily(self.light_off, self.args["off_time"]
+#    time_off = self.parse_time(self.args["off_time"])
+    time_on = datetime.time(20, 00, 0)
+    time_off = datetime.time(23, 00, 00)
+#    self.run_at_sunset(self.light_on, offset=300)
+    self.run_daily(self.light_on, time_on)
+    self.run_daily(self.light_off, time_off)
 
   def light_on(self, kwargs):
     self.turn_on(self.args["light"], brightness=self.args["brightness"])
