cd39c60048ff43038b372d37186a6808f74d1299
Calle <calle.strom@hotmail.com>
2019-02-09 18:24:29 +0100

    refactoring

--- appdaemon/apps/hello.py
+++ appdaemon/apps/hello.py
@@ -1,5 +1,7 @@
 import appdaemon.plugins.hass.hassapi as hass
 
+from typing import Callable
+
 #
 # Hello World App
 #
@@ -11,3 +13,15 @@ class HelloWorld(hass.Hass):
     def initialize(self):
         self.log("Hello from AppDaemon")
         self.log("You are now ready to run Apps!")
+
+        bool_exist = self.get_state("input_boolean.AD_{}".format(self.name))
+        if bool_exist is None:
+            friendly_name = self.name.replace('_', ' ').title()
+            self.log("AppName: {}".format(friendly_name))
+            self.attributes = {}
+            self.attributes['friendly_name'] = friendly_name
+            # self.attributes['icon'] = "mdi:language-python"
+            self.set_state("input_boolean.AD_{}".format(self.name),
+                           state='on', 
+                           attributes=self.attributes)
+            self.call_service("logbook/log", name="{}".format(self.name), message='Init bool')
